c6c2e7bd27e5fa023d35ab686ae164baf43007b4
Isaac <bennisaac24@gmail.com>
2025-03-04 21:53:04 +0300

    updated

--- lib/models.py
+++ lib/models.py
@@ -27,3 +27,12 @@ class Dev(Base):
 
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+    dev_id = Column(Integer(), ForeignKey('companies.id'))
